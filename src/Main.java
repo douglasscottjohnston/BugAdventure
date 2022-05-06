902a1291dda8f86268db2f5e7ee0046f5aeb4c9d
Douglas <douglasscottjohnston@gmail.com>
2022-05-05 23:04:54 -0700

    changed how Room works

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import Model.Dungeon;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Dungeon dungeon = new Dungeon();
 
-        System.out.println(dungeon.toString());
     }
 }

86bb912a45595f78ef529af5a1b2360a2b239920
Cameron Lempitsky <cameronlempitsky@gmail.com>
2022-05-30 23:23:01 -0700

    changed bug stats

--- src/Model/Bugs/Ant.java
+++ src/Model/Bugs/Ant.java
@@ -1,12 +1,12 @@
 package Model.Bugs;
 
 public class Ant extends HeroBug {
-    private static final int MY_HEALTH = 10000;
-    private static final int MY_ORIGINAL_HEALTH = 10000;
+    private static final int MY_HEALTH = 600;
+    private static final int MY_ORIGINAL_HEALTH = 600;
     private static final int MY_DEFENCE = 30;
-    private static final Attack MY_ATTACK = new Attack("bite","",65, 100, false);
-    private static final Attack MY_SPECIAL_ATTACK = new Attack("Super bite","",80, 80, false);
-    private static final int MY_SPEED = 5;
+    private static final Attack MY_ATTACK = new Attack("bite","",70, 100, false);
+    private static final Attack MY_SPECIAL_ATTACK = new Attack("Super bite","",90, 80, false);
+    private static final int MY_SPEED = 4;
     private static final int MY_CHANCE_TO_DODGE = 30;
     public Ant(final String theName) {
         super(MY_ATTACK, MY_SPECIAL_ATTACK, MY_HEALTH,MY_ORIGINAL_HEALTH, MY_DEFENCE, MY_SPEED, MY_CHANCE_TO_DODGE, theName);
--- src/Model/Bugs/PillBug.java
+++ src/Model/Bugs/PillBug.java
@@ -1,12 +1,12 @@
 package Model.Bugs;
 
 public class PillBug extends HeroBug {
-    private static final int MY_HEALTH = 100;
-    private static final int MY_ORIGINAL_HEALTH = 100;
-    private static final int MY_DEFENCE = 70;
-    private static final Attack MY_ATTACK = new Attack("bite","",40, 100, false);
-    private static final Attack MY_SPECIAL_ATTACK = new Attack("Roll Slam","",80, 70, false);
-    private static final int MY_SPEED = 5;
+    private static final int MY_HEALTH = 450;
+    private static final int MY_ORIGINAL_HEALTH = 450;
+    private static final int MY_DEFENCE = 50;
+    private static final Attack MY_ATTACK = new Attack("bite","",60, 100, false);
+    private static final Attack MY_SPECIAL_ATTACK = new Attack("Roll Slam","",80, 85, false);
+    private static final int MY_SPEED = 3;
     private static final int MY_CHANCE_TO_DODGE = 20;
     public PillBug(final String theName) {
         super(MY_ATTACK, MY_SPECIAL_ATTACK, MY_HEALTH, MY_ORIGINAL_HEALTH, MY_DEFENCE, MY_SPEED, MY_CHANCE_TO_DODGE, theName);
